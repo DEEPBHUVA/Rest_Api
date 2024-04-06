d0627ba0c0b59ad6c30b3eab064d6c25ccd7a456
Deep Bhuva <bhuvadeep75@gmail.com>
2024-04-06 13:13:30 +0530

    Second Commit

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -14,17 +14,17 @@ Router.get("/:id",async(req, res) => {
 
 Router.post("/",async(req, res) => {
     const data = await User.create(req.body);
-    res.json(data);
+    res.json("Ueser Inserted Successfully!!",data);
 })
 
 Router.put("/:id",async(req, res) => {
     const data = await User.findByIdAndUpdate(req.params.id, req.body);
-    res.json(data);
+    res.json("Ueser Updated Successfully!!",data);
 })
 
 Router.delete("/:id",async(req, res) => {
     const data = await User.findByIdAndDelete(req.params.id);
-    res.json(data);
+    res.json("Ueser Deleted Successfully!!",data);
 })
 
 module.exports = Router;
