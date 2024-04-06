f2770566fdff4a80874d6e8341ef0702dd5e31a2
Deep Bhuva <bhuvadeep75@gmail.com>
2024-04-06 13:43:52 +0530

    Third Commit

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -12,19 +12,34 @@ Router.get("/:id",async(req, res) => {
     res.json(data);
 });
 
-Router.post("/",async(req, res) => {
-    const data = await User.create(req.body);
-    res.json("Ueser Inserted Successfully!!",data);
-})
+Router.post("/", async (req, res) => {
+    try {
+        const data = await User.create(req.body);
+        res.json({ message: "User Inserted Successfully!!", data });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 Router.put("/:id",async(req, res) => {
-    const data = await User.findByIdAndUpdate(req.params.id, req.body);
-    res.json("Ueser Updated Successfully!!",data);
+    try{
+        const data = await User.findByIdAndUpdate(req.params.id, req.body);
+        res.json({ message: "Ueser Updated Successfully!!", data });
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 })
 
 Router.delete("/:id",async(req, res) => {
-    const data = await User.findByIdAndDelete(req.params.id);
-    res.json("Ueser Deleted Successfully!!",data);
+    try{
+        const data = await User.findByIdAndDelete(req.params.id);
+        res.json({ message: "Ueser Deleted Successfully!!", data });
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+   
 })
 
 module.exports = Router;
